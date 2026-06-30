8ed4de63587abf33070c56a6dd2722e854ab6c06
suni72 <sunidhichandra@google.com>
2026-06-30 17:48:09 +0000

    test: Add unit tests for namespace strategies and fix getFileInfo bugs

--- client/src/test/java/com/google/cloud/gcs/analyticscore/client/GcsFileSystemImplTest.java
+++ client/src/test/java/com/google/cloud/gcs/analyticscore/client/GcsFileSystemImplTest.java
@@ -214,6 +214,9 @@ class GcsFileSystemImplTest {
             .setSize((long) content.length())
             .setContentGeneration(12345L) // A sample generation ID
             .build();
+    lenient()
+        .when(mockClient.getBucketProperties(TEST_BUCKET))
+        .thenReturn(BucketProperties.create(false));
     when(mockClient.getGcsItemInfo(eq(itemId))).thenReturn(mockItemInfo);
 
     GcsFileInfo fileInfo = gcsFileSystem.getFileInfo(gcsPath);
@@ -234,13 +237,16 @@ class GcsFileSystemImplTest {
     GcsItemId nonExistentItemId =
         GcsItemId.builder().setBucketName(TEST_BUCKET).setObjectName("non-existent-object").build();
     URI nonExistentPath = new URI("gs://" + TEST_BUCKET + "/non-existent-object");
+    lenient()
+        .when(mockClient.getBucketProperties(TEST_BUCKET))
+        .thenReturn(BucketProperties.create(false));
     when(mockClient.getGcsItemInfo(eq(nonExistentItemId)))
         .thenThrow(new IOException("Object not found:" + nonExistentItemId));
 
     IOException e =
         assertThrows(IOException.class, () -> gcsFileSystem.getFileInfo(nonExistentPath));
 
-    assertThat(e).hasMessageThat().contains("Object not found:" + nonExistentItemId);
+    assertThat(e).hasMessageThat().contains("File not found: " + nonExistentItemId);
   }
 
   @Test
@@ -290,6 +296,9 @@ class GcsFileSystemImplTest {
             .setSize((long) content.length())
             .setContentGeneration(12345L) // A sample generation ID
             .build();
+    lenient()
+        .when(mockClient.getBucketProperties(TEST_BUCKET))
+        .thenReturn(BucketProperties.create(false));
     when(mockClient.getGcsItemInfo(eq(itemId))).thenReturn(mockItemInfo);
 
     GcsFileInfo fileInfo = gcsFileSystem.getFileInfo(itemId);
@@ -308,13 +317,16 @@ class GcsFileSystemImplTest {
   void getFileInfo_withNonExistentItemId_shouldThrowException() throws IOException {
     GcsItemId nonExistentItemId =
         GcsItemId.builder().setBucketName(TEST_BUCKET).setObjectName("non-existent-object").build();
+    lenient()
+        .when(mockClient.getBucketProperties(TEST_BUCKET))
+        .thenReturn(BucketProperties.create(false));
     when(mockClient.getGcsItemInfo(eq(nonExistentItemId)))
         .thenThrow(new IOException("Object not found:" + nonExistentItemId));
 
     IOException e =
         assertThrows(IOException.class, () -> gcsFileSystem.getFileInfo(nonExistentItemId));
 
-    assertThat(e).hasMessageThat().contains("Object not found:" + nonExistentItemId);
+    assertThat(e).hasMessageThat().contains("File not found: " + nonExistentItemId);
   }
 
   @Test
